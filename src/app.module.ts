52773fff48d590f6421a6640215f1127a7917dc0
gibarel <gibarel@gmail.com>
2025-09-15 12:19:17 -0300

    Configuro TypeORM y agrego .gitignore con base incluida

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DepartamentosModule } from './departamentos/departamentos.module';
 
 @Module({
-  imports: [],
+  imports: [DepartamentosModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+TypeOrmModule.forRoot({
+  type: 'sqlite',
+  database: 'data/sistema_tracking.db',
+  autoLoadEntities: true,
+  synchronize: true,
+})
